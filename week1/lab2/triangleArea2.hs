c38c510318feed6a66ee8144f240ab6995182186
Ben <benthedev@outlook.com>
2019-10-02 16:39:49 +0100

    triangleArea2.hs doesnt work but progress was made

---
+++ week1/lab2/triangleArea2.hs
@@ -0,0 +1,16 @@
+isTriangle :: Float -> Float -> Float -> Bool
+isTriangle a b c 
+   | a + b > c = True
+   | b + c > a = True
+   | c + a > b = True
+   | otherwise = False
+
+ -- Calculate the area of a triangle
+whatIsS :: Float -> Float -> Float -> Float
+whatIsS a b c = (a + b + c) / 2.0
+
+triangleArea :: Float -> Float -> Float -> Float
+triangleArea a b c
+   | isTriangle a b c == False = error "Not a triangle!"
+   | otherwise = sqrt(s*(s-a)*(s-b)*(s-c))
+      where s = whatIsS a b c
