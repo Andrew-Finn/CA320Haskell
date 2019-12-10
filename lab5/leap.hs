61403b16be439f2488fd4be87e66b553f62975da
Ben <benthedev@outlook.com>
2019-12-10 15:30:10 +0000

    lab 5 leap func

---
+++ lab5/leap.hs
@@ -0,0 +1,13 @@
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+    deriving (Enum, Show)
+
+data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
+    deriving (Enum, Read)
+
+type Date = (Int, Month, Int)
+
+leap :: Int -> Bool
+leay yr
+   | yr 'mod' 100 == 0 = yr 'mod 400 == 0
+   | yr 'mod' 4 == 0 = True
+   | otherwise == True  
