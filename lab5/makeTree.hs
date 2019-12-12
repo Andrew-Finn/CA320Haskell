2144050d52e21e45ba54fd94d9ff4b1d643c4f4a
Ben <benthedev@outlook.com>
2019-12-12 10:38:16 +0000

    Actually finished lab 5

---
+++ lab5/makeTree.hs
@@ -0,0 +1,13 @@
+data Tree a = Null
+    | Node a (Tree a) (Tree a)
+    deriving (Read, Show)
+
+addNode :: Ord a => a -> Tree a -> Tree a
+addNode n Null = Node n Null Null
+addNode n (Node nb b1 b2)
+    | n < nb = Node nb (addNode n b1) b2
+    | otherwise = Node nb b1 (addNode n b2)
+
+makeTree :: Ord a => [a] -> Tree a
+makeTree [] = Null
+makeTree (h:t) = addNode h (makeTree t)
